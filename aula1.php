88dd65c8f7997e10e810e5a30b53ed5e87638475
Maria Roberta <mariarobertaprado@outlook.com>
2022-02-17 22:39:34 -0300

    new: learning php basics

---
+++ aula1.php
@@ -0,0 +1,88 @@
+<?php
+
+
+function testing_variables_1() {
+    
+    $nome = "Maria Roberta";
+    $idade = 20;
+    $senha = 'Integrado$nome';
+    $senha1 = "Integrado$nome";
+
+    echo " <h5> A aluna " .$nome." tem a idade de ".$idade." anos </h5>";
+
+    echo " <h5> A aluna {$nome} tem a idade de {$idade} anos </h5>";
+    
+    //Aspas simples não deixam que $nome se torne uma variavel
+    
+    echo "<hr>  A senha sem variavel eh <h5> {$senha} </h5> a sehnha com variavel eh <h5> {$senha1} </h5> <hr>";
+    
+}
+
+function salario_2() {
+    $nome = "Maria Roberta";
+
+    $salario = 1000.99;
+    $salarioFormatado = number_format($salario, 2, ",", ".");
+    $imposto = ( $salario * 10 ) / 100;
+    $impostoFormatado = number_format($imposto, 2, ",", ".");
+
+
+    echo "<p> {$nome} ganha {$salarioFormatado}<hr> ";
+    echo "<p> {$nome} paga {$impostoFormatado} de imposto <hr> ";
+}
+
+function myArray_3() {
+    $cars = array("Volvo","BMW","Toyota");
+    $pessoas = array ('1' => 'Maria', '2' => 'Joao');   
+
+    var_dump($cars);
+    echo "<br>";
+
+    var_dump($pessoas);
+
+    echo "<br>  Meu array: ".$cars[0];
+    echo "<br>";
+
+    echo "<br>  Meu array: ".$pessoas['1'];
+    echo "<br>";
+    print_r($cars);
+    echo "<br>";
+
+
+    foreach($cars as $p) {
+        echo "<p> O carro é: {$p} </p>";
+    }
+
+    foreach($pessoas as $key => $value) {
+        echo "<p> a pessoa é: {$value} e o id eh {$key} </p>";
+    }
+
+    
+}
+//salario_2();
+//testing_variables_1();
+
+myArray_3();
+
+/*
+class Car {
+
+    public $color;
+    public $model;
+    public function __construct($color, $model) {
+      $this->color = $color;
+      $this->model = $model;
+    }
+    public function message() {
+      return "My car is a " . $this->color . " " . $this->model . "!";
+    }
+  }
+  
+  $myCar = new Car("black", "Volvo");
+  echo $myCar -> message();
+  echo "<br>";
+  $myCar = new Car("red", "Toyota");
+  echo $myCar -> message();
+
+
+*/
